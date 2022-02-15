7c9924bff723c938d1d82ec30a619ee2813273d2
${MrDo359} <${msdo2021@foxmail.com}>
2022-02-15 20:27:06 +0800

    项目自身配置

--- demo-module-1/src/com/he/MainTest.java
+++ demo-module-1/src/com/he/MainTest.java
@@ -3,5 +3,7 @@ package com.he;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+
+        System.out.println("git change");
     }
 }
